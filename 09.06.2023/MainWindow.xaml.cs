de90d3a3ec4d0b9b588cd1525d7ddac2bca0dbd3
flexxdarker <ksenzhakmax@gmail.ocm>
2023-06-09 20:53:25 +0300

    completed

---
+++ 09.06.2023/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _09._06._2023
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ViewModel viewmodel = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = viewmodel;
+        }
+    }
+    [PropertyChanged.AddINotifyPropertyChangedInterface()]
+    public class ViewModel
+    { 
+        public Color color => Color.FromArgb(alpha, red, green, blue);
+        public byte alpha { get; set; }
+        public byte red { get; set; }
+        public byte green { get; set; }
+        public byte blue { get; set; }
+        public ViewModel()
+        {
+            
+        }
+    }
+
+}
